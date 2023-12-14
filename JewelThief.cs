f49a3c79e0ead4b2ec12698b06efb13709052997
Kolvinkst <einbenutzer1998@gmail.com>
2023-12-14 08:40:45 +0700

    Добавьте файлы проекта.

---
+++ JewelThief.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeWithJewels
+{
+    internal class JewelThief : LockSmith
+    {
+        private string stolenJewels;
+        protected override void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine($"I'm stealing the jewels! I stole: {stolenJewels}");
+        }
+    }
+}
